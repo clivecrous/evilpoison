9d91a0c6a4baee2c6e9cf8d77de121e96aad5a55
Clive Crous <clive@crous.co.za>
2006-10-21 16:03:31 +0000

    revert accidentally committed changes in previous [169] commit

--- keymap.h
+++ keymap.h
@@ -1,57 +1,30 @@
 #ifndef _KEYMAP_H
 #define _KEYMAP_H
 
-#define PREFIX_KEY XK_e
-#define PREFIX_MOD ControlMask
-
-enum KEYVALS {
-    KEY_NONE,
-    KEY_PREFIX,
-    KEY_MOVEWIN,
-    KEY_NEXT,
-    KEY_NEW,
-    KEY_TOPLEFT,
-    KEY_TOPRIGHT,
-    KEY_BOTTOMLEFT,
-    KEY_BOTTOMRIGHT,
-    KEY_LEFT,
-    KEY_RIGHT,
-    KEY_DOWN,
-    KEY_UP,
-    KEY_RESIZELEFT,
-    KEY_RESIZERIGHT,
-    KEY_RESIZEDOWN,
-    KEY_RESIZEUP,
-
-    KEY_MOUSEDRAG,
-    KEY_MOUSESWEEP,
-
-    KEY_LOWER,
-    KEY_INFO,
-    KEY_MAXVERT,
-    KEY_MAXHORIZ,
-    KEY_MAX,
-
-    KEY_VSPLIT,
-    KEY_HSPLIT,
-
+#define KEY_NEXT	XK_Tab
+#define KEY_NEW		XK_Return
+#define KEY_TOPLEFT	XK_y
+#define KEY_TOPRIGHT	XK_u
+#define KEY_BOTTOMLEFT	XK_b
+#define KEY_BOTTOMRIGHT	XK_n
+#define KEY_LEFT	XK_h
+#define KEY_RIGHT	XK_l
+#define KEY_DOWN	XK_j
+#define KEY_UP		XK_k
+#define KEY_LOWER	XK_Insert
+#define KEY_ALTLOWER	XK_KP_Insert
+#define KEY_INFO	XK_i
+#define KEY_MAXVERT	XK_equal
+#define KEY_MAX		XK_x
 #ifdef VWM
-    KEY_FIX,
-    KEY_PREVDESK,
-    KEY_NEXTDESK,
-    KEY_DESK1,
-    KEY_DESK2,
-    KEY_DESK3,
-    KEY_DESK4,
-    KEY_DESK5,
-    KEY_DESK6,
-    KEY_DESK7,
-    KEY_DESK8,
+# define KEY_FIX		XK_f
+# define KEY_PREVDESK	XK_Left
+# define KEY_NEXTDESK	XK_Right
 #endif
-    KEY_KILL
-};
-
-#define KEY_TO_VDESK(key) ((key) - KEY_DESK1)
-#define valid_vdesk(v) ((unsigned)(v) < KEY_TO_VDESK(KEY_DESK8))
+/* Mixtures of Ctrl, Alt an Escape are used for things like VMWare and
+ * XFree86/Cygwin, so the KILL key is an option in the Makefile  */
+#ifndef KEY_KILL
+#define KEY_KILL	XK_Escape
+#endif
 
 #endif
