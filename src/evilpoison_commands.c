9d07b92819608ba909c726c75fcdad1649366517
Clive Crous <clive@crous.co.za>
2006-11-06 15:11:24 +0000

    Add EvilPoison seperated command infrastructure with: set/unset. No tests yet ... these to follow

---
+++ src/evilpoison_commands.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "command.h"
+#include "settings.h"
+#include "evilpoison_commands.h"
+
+char *evilpoison_command_set(char *commandline);
+char *evilpoison_command_unset(char *commandline);
+
+char *evilpoison_command_set(char *commandline)
+{
+  char *newcommandline = malloc( strlen( commandline ) + 1 );
+  strcpy( newcommandline, commandline );
+
+  char *setting = newcommandline;
+  while (*setting && *setting==' ' ) setting++;
+
+  char *value = setting;
+  while (*value && *value!=' ' ) value++;
+  while (*value && *value==' ' ) { *value='\0'; value++; };
+
+  settings_set( setting, value );
+
+  free( newcommandline );
+
+  return 0;
+}
+
+char *evilpoison_command_unset(char *commandline)
+{
+  settings_unset( commandline );
+
+  return 0;
+}
+
+void evilpoison_commands_init( void )
+{
+  command_init();
+
+  command_assign( "set", evilpoison_command_set );
+}
---
+++ src/evilpoison_commands.h
@@ -0,0 +1,4 @@
+/** Initialize evilpoison commands.
+ * This function sets up all default internal commands.
+ */
+void evilpoison_commands_init( void );
